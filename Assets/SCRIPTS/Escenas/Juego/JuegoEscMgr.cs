f760db28bf6d49abb27e8616ce2796c9405efed8
Nicolas Leon <nicolasaleon05@gmail.com>
2025-09-26 19:39:44 -0300

    Initial commit

---
+++ Assets/SCRIPTS/Escenas/Juego/JuegoEscMgr.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using System.Collections;
+
+public class JuegoEscMgr : MonoBehaviour 
+{
+	bool JuegoFinalizado = false;
+	public float TiempoEsperaFin = 25;//tiempo que espera la aplicacion para volver al video introductorio desp de terminada la partida
+	float Tempo = 0;
+	
+	bool JuegoIniciado = false;
+	public float TiempoEsperaInicio = 120;//tiempo que espera la aplicacion para volver al video introductorio desp de terminada la partida
+	float Tempo2 = 0;
+	
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(JuegoFinalizado)
+		{
+			Tempo += Time.deltaTime;
+			if(Tempo > TiempoEsperaFin)
+			{
+				Tempo = 0;
+				Application.LoadLevel(0);
+                //Reemplazar por un scene manager
+            }
+        }
+		
+		if(!JuegoIniciado)
+		{
+			Tempo2 += Time.deltaTime;
+			if(Tempo > TiempoEsperaInicio)
+			{
+				Tempo2 = 0;
+				Application.LoadLevel(0);
+				//Reemplazar por un scene manager
+			}
+		}		
+		
+		if(Input.GetKeyDown(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
+		
+		//reinicia
+		if(Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+		{
+			Application.LoadLevel(Application.loadedLevel);
+            //Reemplazar por un scene manager
+        }
+    }
+	
+	//---------------------------------------------------//
+	
+	public void JuegoFinalizar()
+	{
+		JuegoFinalizado = true;
+	}
+	
+	public void JuegoIniciar()
+	{
+		JuegoIniciado = true;
+	}
+}
